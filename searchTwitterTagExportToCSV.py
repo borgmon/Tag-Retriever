d2a8e7a930effdaa7a59017b7cf1031886a6754c
Unknown <borgmon7@gmail.com>
2017-11-06 08:26:59 -0800

    Fix append result bug

--- searchTwitterTagExportToCSV.py
+++ searchTwitterTagExportToCSV.py
@@ -11,7 +11,7 @@ def getAllHashtags(list):
     hashStr = ''
     for hashes in list:
         hashStr += hashes['text']
-        hashStr += ', '
+        hashStr += ' '
 
     return hashStr
 
@@ -27,19 +27,26 @@ def getLocation(place):
 api = TwitterAPI(consumer_key, consumer_secret,
                  access_token_key, access_token_secret)
 
-r = api.request('search/tweets', {'q': '#pizza', 'count': '15'})
+r = api.request('search/tweets', {'q': '#cat', 'count': '15'})
 
 data = []
 
 for item in r:
     data.append(item)
 
-results = [[tweet['user']['screen_name'], tweet['created_at'], getAllHashtags(tweet['entities']['hashtags']),
+results = [[tweet['user']['screen_name'], tweet['created_at'].replace('+0000 ', ''), getAllHashtags(tweet['entities']['hashtags']),
             getLocation(tweet['place']), tweet['text'].encode("utf-8")] for tweet in data]
 
-with open('output.csv', 'wb') as f:
+with open('TwitterOutput.csv', 'r+a') as f:
     writer = csv.writer(f)
-    writer.writerow(["screen_name", "created_at", "hashtags", "place", "text"])
-    writer.writerows(results)
+    f.seek(0)  # ensure you're at the start of the file..
+    first_char = f.read(1)  # get the first character
+    if not first_char:
+        writer.writerow(["screen_name", "created_at",
+                         "hashtags", "place", "text"])
+        writer.writerows(results)
+    else:
+        writer.writerows(results)
 
 pass
+print 'finished!'
